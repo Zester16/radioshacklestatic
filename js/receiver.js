d363ef3f227d1151db80a3e452da708155df88b5
Zester16 <88012803+Zester16@users.noreply.github.com>
2022-06-30 14:50:16 +0530

    Delete js directory

--- js/receiver.js
+++
@@ -1,6 +0,0 @@
-const context = cast.framework.CastReceiverContext.getInstance();
-const playerManager = context.getPlayerManager();
-// Optimizing for smart displays
-//const touchControls = cast.framework.ui.Controls.getInstance();
-
-context.start();
