33558420e3d13fee2f1b93ce2220718f8ab690f2
Zester16 <88012803+Zester16@users.noreply.github.com>
2022-06-30 14:48:01 +0530

    Add files via upload

---
+++ js/receiver.js
@@ -0,0 +1,6 @@
+const context = cast.framework.CastReceiverContext.getInstance();
+const playerManager = context.getPlayerManager();
+// Optimizing for smart displays
+//const touchControls = cast.framework.ui.Controls.getInstance();
+
+context.start();
